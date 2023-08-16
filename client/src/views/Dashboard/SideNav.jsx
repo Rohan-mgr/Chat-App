99e70dc14a87f3a77b4c130e2e1a00bf1b23e737
Rohan Magar <rohan.magar.415@gmail.com>
2023-08-16 18:22:14 +0545

    finished search UI

--- client/src/views/Dashboard/SideNav.jsx
+++ client/src/views/Dashboard/SideNav.jsx
@@ -3,11 +3,28 @@ import { _remove } from "../../utils/storage";
 import { useNavigate } from "react-router-dom";
 import useFetchUsers from "../../hooks/useFetchUsers";
 import NameInitials from "../../components/common/NameInitials";
-import { BiLogOutCircle } from "react-icons/bi";
+import { useFormik } from "formik";
+import { BsSearch } from "react-icons/bs";
+import { searchUsers } from "../../services/user";
 
 function SideNav() {
   const navigate = useNavigate();
   const { isLoading, users } = useFetchUsers();
+
+  const formik = useFormik({
+    initialValues: {
+      search: "",
+    },
+    onSubmit: async (values) => {
+      try {
+        const response = await searchUsers(values);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+        throw new Error(error);
+      }
+    },
+  });
 
   const handleLogout = () => {
     _remove("auth");
@@ -23,16 +40,30 @@ function SideNav() {
           </Button>
         </div>
       </div>
-      {users.map((user) => {
-        return (
-          <NameInitials
-            handleClick={() => navigate(`chat/${user?._id}`, { state: user })}
-            key={user?._id}
-            name={user?.fullName}
-            message={"This is a message."}
+      <div className="side__nav__search">
+        <form onSubmit={formik.handleSubmit}>
+          <BsSearch />
+          <input
+            type="text"
+            placeholder="Search People"
+            name="search"
+            value={formik.values.search}
+            onChange={formik.handleChange}
           />
-        );
-      })}
+        </form>
+      </div>
+      <div className="side__nav__users">
+        {users.map((user) => {
+          return (
+            <NameInitials
+              handleClick={() => navigate(`chat/${user?._id}`, { state: user })}
+              key={user?._id}
+              name={user?.fullName}
+              message={"This is a message."}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
